1c22b35672ef15b1b1d438fc4f140f42afe99249
CrescentLuo <luozheng@picb.ac.cn>
2017-02-16 13:49:49 +0800

    Bug fix, Remove -m 2 parameter

--- scripts/Tophat2_Mapping_Command.sh
+++ scripts/Tophat2_Mapping_Command.sh
@@ -4,11 +4,13 @@
 job_name=$1
 echo `date` >> ${job_name}.log
 echo "Run job list $job_name" >> ${job_name}.log
+sleep 1
 function do_job {
-    echo "starting job $1" >> ${job_name}.log
     echo `date` >> ${job_name}.log
+    echo "starting job $1" >> ${job_name}.log
     fastq_file=${1##*/}
-    tophat2 -g 1 -a 6 --microexon-search -m 2 -p 10 -G /picb/rnomics1/database/Mouse/mm10/annotation/knownGene.gtf -o ./${fastq_file%%.*}  /picb/rnomics1/database/Mouse/mm10/genome/mm10_all $1 &> ${fastq_file%%.*}_tophat2.log
+    tophat2 -g 1 -a 6 --microexon-search -p 10 -G /picb/rnomics1/database/Mouse/mm10/annotation/knownGene.gtf -o ./${fastq_file%%.*}  /picb/rnomics1/database/Mouse/mm10/genome/mm10_all $1 &> ${fastq_file%%.*}_tophat2.log
+    sleep 1
 }
 #ls todo files first to generate job list
 todo_array=($(cat $1))
@@ -29,6 +31,7 @@ for((i=0; i<${#todo_array[*]}; i++));do
         do_job ${todo_array[$i]} && {
             echo `date` >> ${job_name}.log
             echo finish job ${todo_array[$index]} >> ${job_name}.log
+            sleep 1
         } || {
             echo job ${todo_array[$i]} error >> ${job_name}.log
         }
