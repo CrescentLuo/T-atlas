e375fd08634c0b341f019d1547b08f5a21dd4fc8
CrescentLuo <luozheng@picb.ac.cn>
2017-02-20 16:03:13 +0800

    FetchByColumn

--- scripts/FetchByColumn.py
+++ scripts/FetchByColumn.py
@@ -6,7 +6,7 @@ with open(sys.argv[1]) as index_file:
         index_set.add(sline[0])
 with open(sys.argv[2]) as data_file:
     file_header = data_file.readline()
-    print file_header
+    print file_header.rstrip()
     for line in data_file:
         sline = line.rstrip().split()
         if sline[0] in index_set:
