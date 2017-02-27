ddf4f832fae68156534b3802448e0dc9ba70a401
CrescentLuo <luozheng@picb.ac.cn>
2017-02-27 15:25:48 +0800

    BugFIxed

--- scripts/FetchByColumn.py
+++ scripts/FetchByColumn.py
@@ -1,13 +1,14 @@
+"""Fetch by Colunmn"""
 import sys
-index_set = set()
+INDEX_SET = set()
 with open(sys.argv[1]) as index_file:
     for line in index_file:
         sline = line.rstrip().split()
-        index_set.add(sline[0])
+        INDEX_SET.add(sline[0])
 with open(sys.argv[2]) as data_file:
-    file_header = data_file.readline()
-    print file_header.rstrip()
+    FILE_HEADER = data_file.readline()
+    print FILE_HEADER.rstrip()
     for line in data_file:
         sline = line.rstrip().split()
-        if sline[0] in index_set:
+        if sline[0] in INDEX_SET:
             print line.rstrip()
--- scripts/Tophat2_Mapping_Command.sh
+++ scripts/Tophat2_Mapping_Command.sh
@@ -5,16 +5,20 @@ job_name=$1
 echo `date` >> ${job_name}.log
 echo "Run job list $job_name" >> ${job_name}.log
 sleep 1
-function tophat2_mapping {
+function tophat2_mapping_paired_end {
     echo `date` >> ${job_name}.log
     echo "starting job $1" >> ${job_name}.log
     fastq_file=${1##*/}
     tophat2 -g 1 -a 6 --microexon-search -p 10 -G /picb/rnomics1/database/Mouse/mm10/annotation/knownGene.gtf -o ./${fastq_file%%.*}  /picb/rnomics1/database/Mouse/mm10/genome/mm10_all $1 &> ${fastq_file%%.*}_tophat2.log
     sleep 1
 }
-function hisat2_mapping {
-        echo `date` >> ${job_name}.log
-        echo "starting jobs"
+function hisat2_mapping_paired_end {
+    echo `date` >> ${job_name}.log
+    echo "starting job $1" >> ${job_name}.log
+    fastq_file=${1}
+    # -x hisat2 index dir
+    # -1 -2 paired-end fastq files
+    hisat2 -x hg19_index -1 light-1_R1.fq -2 light-1_R2.fq -S light-1.sam -p 16 2>light-1.log
 }
 #ls todo files first to generate job list
 todo_array=($(cat $1))
@@ -32,7 +36,7 @@ done >&6
 for((i=0; i<${#todo_array[*]}; i++));do
     read -u6
     {
-        tophat2_mapping ${todo_array[$i]} && {
+        tophat2_mapping_paired_end ${todo_array[$i]} && {
             echo `date` >> ${job_name}.log
             echo finish job ${todo_array[$i]} >> ${job_name}.log
             sleep 1
