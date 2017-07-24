9de1a42caafdabb23d6d1f3a5eb9516fc957f256
CrescentLuo <crescentluozheng@gmail.com>
2017-07-24 21:18:50 +0800

    Kmer

--- Kmer.py
+++ Kmer.py
@@ -2,13 +2,14 @@ import sys
 import argparse
 import itertools
 from pybedtools import BedTool
-from multiprocessing import Process,Lock
+from multiprocessing import Process,Lock,Pool
 
 parser = argparse.ArgumentParser(description='get fasta from bed files and count ATCG percentage')
 parser.add_argument('-b', '--bed', required=True, help='input bed file')
 parser.add_argument('-f', '--fasta', required=True, help='fasta file')
 parser.add_argument('-k', '--repeat', required=True, help='set K for kmer')
 parser.add_argument('-s', '--split', default=True, help='split to get mature sequence')
+parser.add_argument('-p', '--process', default=5, help='number of processes')
 args = parser.parse_args()
 
 def kmerPermutation(K):
@@ -41,8 +42,11 @@ def kmerFreq(lock, K, line):
 
 with open(args.bed) as bedFile:
     lock = Lock()
-    K = args.repeak
+    pool = multiprocessing.Pool(processes = int(args.process))
+    K = int(args.repeak)
     kmer_dict = kmerPermutation(K)
     fasta = BedTool(args.fasta)
     for line in bedFile:
-        Process(target=kmerFreq, args=(lock, K, line)).start()
+        pool.apply_async(target=kmerFreq, args=(lock, K, line)).start()
+    pool.close()
+    pool.join()
