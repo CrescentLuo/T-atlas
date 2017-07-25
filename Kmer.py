4b60583789a314b2da5b90f4ee65a8abd65053cb
CrescentLuo <crescentluozheng@gmail.com>
2017-07-25 16:34:55 +0800

    Kmer

--- Kmer.py
+++ Kmer.py
@@ -1,8 +1,8 @@
 import sys
 import argparse
 import itertools
-import pybedtools
-from pybedtools import BedTool
+from Bio import SeqIO
+from Bio.Seq import Seq
 
 parser = argparse.ArgumentParser(description='get fasta from bed files and count ATCG percentage')
 parser.add_argument('-b', '--bed', required=True, help='input bed file')
@@ -10,6 +10,7 @@ parser.add_argument('-f', '--fasta', required=True, help='fasta file')
 parser.add_argument('-k', '--repeat', required=True, help='set K for kmer')
 parser.add_argument('-s', '--split', default=True, help='split to get mature sequence')
 parser.add_argument('-p', '--process', default=5, help='number of processes')
+parser.add_argument('-o', '--overlaping', default=True, help='Count kmer with or without overlapping')
 args = parser.parse_args()
 
 def kmerPermutation(K):
@@ -21,11 +22,19 @@ def kmerPermutation(K):
     return nucbase_kmer_dict
 
 def kmerFreq( K, line):
-    bedline = BedTool(line, from_string=True)
-    fasta = pybedtools.example_filename(args.fasta)
-    get_fasta = bedline.sequence(fi=fasta, split=True, s=True)
-    #print get_fasta
-    #print get_fasta
+    sline = line.rstrip().split()
+    chrom = sline[0]
+    start = sline[1]
+    end = sline[2]
+    exonCnt = int(sline[9])
+    exonlen = sline[10].rstrip(',').split(',')
+    exonS = sline[11].rstrip(',').split(',')
+    strand = sline[5]
+    gene_seq = records[chrom].seq[int(sline[1]):int(sline[2])]
+    spliced_seq = ""
+    for i in range(exonCnt):
+        spliced_seq = spliced_seq + gene_seq[exonS[i]:(exonS[i] + exonlen[i])]
+    print len(spliced_seq),'\t',spliced_seq
     seq =  (open(get_fasta.seqfn).read()).split('\n')[1]
     #print seq
     seq = seq.upper()
@@ -42,9 +51,8 @@ def kmerFreq( K, line):
     kmer_freq = [str(freq) for freq in kmer_freq]  
     print line.split()[3]+'\t'+'\t'.join(kmer_freq)
     #return line.split()[3]+'\t'+'\t'.join(kmer_freq)
-
+records = SeqIO.to_dict(SeqIO.parse(open(args.fasta), 'fasta'))
 with open(args.bed) as bedFile:
-    pybedtools.set_tempdir('./')
     K = int(args.repeat)
     kmer_dict = kmerPermutation(K)
     for line in bedFile:
