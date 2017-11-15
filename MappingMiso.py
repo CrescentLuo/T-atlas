39f6a26f460c99697a850c6f58f474f5b0c3630a
CrescentLuo <crescentluozheng@gmail.com>
2017-11-15 17:13:48 +0800

    mapping event to gff

--- MappingMiso.py
+++ MappingMiso.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import argparse
 from BCBio import GFF
+
 parser = argparse.ArgumentParser(description='This script is written for mapping MISO events id to gene annotation')
 parser.add_argument('-i', '--input', help="Input from miso filter output")
 parser.add_argument('-g', '--gff', help='gene annotaion gff file')
@@ -7,8 +9,14 @@ args = parser.parse_args()
 
 limits = dict(gff_type = ['gene','mRNA'])
 gff_handle = open(args.gff)
+gff_dict = dict()
 for rec in GFF.parse(gff_handle, limit_info=limits):
     for gene_feature in rec.features:
         print gene_feature
         print gene_feature.id, gene_feature.qualifiers['gsymbol'][0]
+        gff_dict[gene_feature.id] = [gene_feature.qualifiers['ensg_id'][0],gene_feature.qualifiers['gsymbol'][0]]
+
+miso_bf = pd.read_table(args.input)
+for event in miso_bf.iterrows():
+    print event['event_name'], print gff_dict[event['event_name']][1], print even['diff']
 
